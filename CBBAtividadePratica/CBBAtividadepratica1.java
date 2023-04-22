c2e8da8e103806638a2b98ce3e0cbabc4a63364f
IagoGOOl <iago.gool@gmail.com>
2023-04-21 22:12:25 -0300

    Atividade CBB questao 1

---
+++ CBBAtividadePratica/CBBAtividadepratica1.java
@@ -0,0 +1,10 @@
+public class CBBAtividadepratica1 {
+    public static void main(String[] args) {
+        int[] array = new int[100];
+        for (int y = 0; y < 100; y++) {
+            array[y] = y;
+            System.out.println(array[y]);
+        }
+
+    }
+}
