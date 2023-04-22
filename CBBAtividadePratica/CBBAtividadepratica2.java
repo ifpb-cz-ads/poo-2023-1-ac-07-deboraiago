63cebdcc4790ef87c1da07a83de4bf3b1f8c037b
IagoGOOl <iago.gool@gmail.com>
2023-04-22 01:50:24 -0300

    CBBAtividadePratica2

---
+++ CBBAtividadePratica/CBBAtividadepratica2.java
@@ -0,0 +1,9 @@
+public class CBBAtividadepratica2 {
+    public static void main(String[] args) {
+        int[] array = new int[100];
+        for (int y = 0; y < 101; y++) {
+            array[y] = y + 1;
+            System.out.println(array[y]);
+        }
+    }
+}
