bfcf2f787a558012c4128144a1c43dd92d32b0b5
IagoGOOl <iago.gool@gmail.com>
2023-04-24 10:11:11 -0300

    CBBAtividadePratica3

---
+++ CBBAtividadePratica/CBBAtividadePatrica3.java
@@ -0,0 +1,10 @@
+public class CBBAtividadePatrica3 {
+    public static void main(String[] args) {
+        int[] array = new int[100];
+        for(int i = 0; i <= array.length; i++){
+            array[i] = i;
+            System.out.println(array[i]);
+        }
+    }
+    
+}
